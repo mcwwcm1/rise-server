77b75998f4c53e25a9cc5fa3d935b7d15f5cd7da
Coffee <isor.graphics@gmail.com>
2021-07-13 19:39:25 +0100

    Implemented totally non-functioning EPA

--- physics/collisiondetection.cpp
+++ physics/collisiondetection.cpp
@@ -161,6 +161,136 @@ bool Simplex::tetrahedron(Float3& direction)
 	return true;
 }
 
+void handleCollision(Shape shape1, Shape shape2, Simplex simplex, CollisionResult& collissionResult)
+{
+	std::vector<Float3> polytope(simplex.begin(), simplex.end());
+	std::vector<size_t>  faces = {
+		0, 1, 2,
+		0, 3, 1,
+		0, 2, 3,
+		1, 3, 2
+	};
+
+	auto [normals, minFace] = GetFaceNormals(polytope, faces);
+
+	Float3 minNormal;
+	float   minDistance = FLT_MAX;
+	
+	while (minDistance == FLT_MAX) {
+		minNormal = normals[minFace].xyz();
+		minDistance = normals[minFace].w;
+ 
+		Float3 s = support(shape1, shape2, minNormal);
+		float sDistance = dot(minNormal, s);
+ 
+		if (abs(sDistance - minDistance) > 0.001f) {
+			minDistance = FLT_MAX;
+
+		std::vector<std::pair<size_t, size_t>> uniqueEdges;
+
+		for (size_t i = 0; i < normals.size(); i++) {
+			if (sameDirection(normals[i].xyz(), s)) {
+				size_t f = i * 3;
+
+				AddIfUniqueEdge(uniqueEdges, faces, f,     f + 1);
+				AddIfUniqueEdge(uniqueEdges, faces, f + 1, f + 2);
+				AddIfUniqueEdge(uniqueEdges, faces, f + 2, f    );
+
+				faces[f + 2] = faces.back(); faces.pop_back();
+				faces[f + 1] = faces.back(); faces.pop_back();
+				faces[f    ] = faces.back(); faces.pop_back();
+
+				normals[i] = normals.back(); normals.pop_back();
+
+				i--;
+			}
+		}
+
+		std::vector<size_t> newFaces;
+		for (auto [edgeIndex1, edgeIndex2] : uniqueEdges) {
+			newFaces.push_back(edgeIndex1);
+			newFaces.push_back(edgeIndex2);
+			newFaces.push_back(polytope.size());
+		}
+			
+		polytope.push_back(s);
+
+		auto [newNormals, newMinFace] = GetFaceNormals(polytope, newFaces);
+
+					float oldMinDistance = FLT_MAX;
+			for (size_t i = 0; i < normals.size(); i++) {
+				if (normals[i].w < oldMinDistance) {
+					oldMinDistance = normals[i].w;
+					minFace = i;
+				}
+			}
+ 
+			if (newNormals[newMinFace].w < oldMinDistance) {
+				minFace = newMinFace + normals.size();
+			}
+ 
+			faces  .insert(faces  .end(), newFaces  .begin(), newFaces  .end());
+			normals.insert(normals.end(), newNormals.begin(), newNormals.end());
+		}
+	}
+ 
+	collissionResult.normal = minNormal;
+	collissionResult.displacement = minDistance + 0.001f;
+}
+
+std::pair<std::vector<Float4>, size_t> GetFaceNormals(
+	const std::vector<Float3>& polytope,
+	const std::vector<size_t>&  faces)
+{
+	std::vector<Float4> normals;
+	size_t minTriangle = 0;
+	float  minDistance = FLT_MAX;
+
+	for (size_t i = 0; i < faces.size(); i += 3) {
+		Float3 a = polytope[faces[i    ]];
+		Float3 b = polytope[faces[i + 1]];
+		Float3 c = polytope[faces[i + 2]];
+
+		Float3 normal = cross(b - a, c - a).normalized();
+		float distance = dot(normal, a);
+
+		if (distance < 0) {
+			normal   *= -1;
+			distance *= -1;
+		}
+
+		normals.emplace_back(normal, distance);
+
+		if (distance < minDistance) {
+			minTriangle = i / 3;
+			minDistance = distance;
+		}
+	}
+
+	return { normals, minTriangle };
+}
+
+void AddIfUniqueEdge(
+	std::vector<std::pair<size_t, size_t>>& edges,
+	const std::vector<size_t>& faces,
+	size_t a,
+	size_t b)
+{
+	auto reverse = std::find(
+		edges.begin(),
+		edges.end(),
+		std::make_pair(faces[b], faces[a])
+	);
+ 
+	if (reverse != edges.end()) {
+		edges.erase(reverse);
+	}
+ 
+	else {
+		edges.emplace_back(faces[a], faces[b]);
+	}
+}
+
 bool Simplex::doSimplex(Float3& direction)
 {
 	switch(pointCount)	{
--- physics/collisiondetection.h
+++ physics/collisiondetection.h
@@ -2,8 +2,17 @@
 // Purpose: Performs the GJK algorithm for finding collisions between arbitrary convex shapes and then performs the EPA to collect collision information
 
 #include "float3.h"
+#include "float4.h"
 #include "shape.h"
 #include <vector>
+
+struct Edge
+{
+	public:
+		int index;
+		float distance;
+		Float3 normal;
+};
 
 struct Simplex
 {
@@ -13,18 +22,22 @@ struct Simplex
 		bool tetrahedron(Float3& direction);
 
 	public:
-		Float3 points [4];
+		std::vector<Float3> points;
 		int pointCount;		
 
 		void pushFront(Float3 point);
 		bool doSimplex(Float3& direction);
+		Edge findClosestEdge();
+
+		auto begin() const { return points.begin(); }
+		auto end()   const { return points.end() - (4 - pointCount); }
 };
 
 struct CollisionResult
 {
 	public:
 		bool collided;
-		Float3 displacement;
+		float displacement;
 		Float3 normal;
 		Float3 point;
 };
@@ -32,5 +45,7 @@ struct CollisionResult
 Float3 support(Shape shape1, Shape shape2, Float3 direction);
 
 CollisionResult checkCollision(Shape shape1, Shape shape2);
+
+CollisionResult handleCollision(Shape shape1, Shape shape2, Simplex simplex);
 
 bool sameDirection(Float3 a, Float3 b);
