b123dadd8bbffef658747616d1e0da967bfdbf58
Psychpsyo <psychpsyo@gmail.com>
2021-08-30 17:38:50 +0200

    fixed transaction.quote() already generating quote marks

--- core/database.cpp
+++ core/database.cpp
@@ -18,13 +18,13 @@ void createPlayer(std::string userID)
 	pqxx::work transaction{*dbConn};
 	
 	//check if the user already exists in the db
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = '" + transaction.quote(userID) + "';");
+	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
 	if (!response.empty()) {
 		return; //TODO: throw error
 	}
 	
 	//insert user into the DB
-	transaction.exec("INSERT INTO player(userid, qp, location) VALUES('" + transaction.quote(userID) + "', 100, 'starterIsland');");
+	transaction.exec("INSERT INTO player(userid, qp, location) VALUES(" + transaction.quote(userID) + ", 100, 'starterIsland');");
 	std::cout << "Created user '" + transaction.quote(userID) + "'";
 	
 	transaction.commit();
@@ -33,7 +33,7 @@ void createPlayer(std::string userID)
 uint64_t getUserQpCount(std::string userID)
 {
 	pqxx::work transaction{*dbConn};
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = '" + transaction.quote(userID) + "';");
+	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
 	
 	return response.begin()["qp"].as<uint64_t>();
 }
@@ -41,19 +41,19 @@ uint64_t getUserQpCount(std::string userID)
 void alterUserQpCount(std::string userID, int64_t delta)
 {
 	pqxx::work transaction{*dbConn};
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = '" + transaction.quote(userID) + "';");
+	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
 	
 	uint64_t currentQP = response.begin()["qp"].as<uint64_t>();
 	currentQP += delta;
 	
-	transaction.exec("UPDATE player SET qp = " + std::to_string(currentQP) + " WHERE userid = '" + transaction.quote(userID) + "';");
+	transaction.exec("UPDATE player SET qp = " + std::to_string(currentQP) + " WHERE userid = " + transaction.quote(userID) + ";");
 	transaction.commit();
 }
 
 std::string getUserLocation(std::string userID)
 {
 	pqxx::work transaction{*dbConn};
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = '" + transaction.quote(userID) + "';");
+	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
 	
 	return response.begin()["location"].c_str();
 }
@@ -61,7 +61,7 @@ std::string getUserLocation(std::string userID)
 void setUserLocation(std::string userID, std::string newLocation)
 {
 	pqxx::work transaction{*dbConn};
-	transaction.exec("UPDATE player SET location = " + transaction.quote(newLocation) + " WHERE userid = '" + transaction.quote(userID) + "';");
+	transaction.exec("UPDATE player SET location = " + transaction.quote(newLocation) + " WHERE userid = " + transaction.quote(userID) + ";");
 }
 
 } // namespace Database
