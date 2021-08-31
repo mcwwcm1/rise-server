959de902baccdec55cf8e580aa0642fbfa80a1f9
Psychpsyo <psychpsyo@gmail.com>
2021-08-31 13:52:15 +0200

    createPlayer() now needs qpCount and location

--- core/database.cpp
+++ core/database.cpp
@@ -13,21 +13,25 @@ void dbConnect()
 	std::cout << "DB Connection Successful" << std::endl;
 }
 
-void createPlayer(const std::string& userID)
+void createPlayer(const std::string& userID, int qpCount, std::string location)
 {
 	pqxx::work transaction{*dbConn};
 	
-	//check if the user already exists in the db
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
-	if (!response.empty()) {
-		return; //TODO: throw error
+	try {
+		//check if the user already exists in the db
+		pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
+		if (!response.empty()) {
+			return; //user already exists so we just don't create another entry.
+		}
+		
+		//insert user into the DB
+		transaction.exec("INSERT INTO player(userid, qp, location) VALUES(" + transaction.quote(userID) + ", " + std::to_string(qpCount) + ", " + transaction.quote(location) + ");");
+		std::cout << "Created user '" + transaction.quote(userID) + "'";
+		
+		transaction.commit();
+	} catch (...) {
+		transaction.abort();
 	}
-	
-	//insert user into the DB
-	transaction.exec("INSERT INTO player(userid, qp, location) VALUES(" + transaction.quote(userID) + ", 100, 'starterIsland');");
-	std::cout << "Created user '" + transaction.quote(userID) + "'";
-	
-	transaction.commit();
 }
 
 uint64_t getUserQpCount(const std::string& userID)
@@ -41,13 +45,12 @@ uint64_t getUserQpCount(const std::string& userID)
 void alterUserQpCount(const std::string& userID, int64_t delta)
 {
 	pqxx::work transaction{*dbConn};
-	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
-	
-	uint64_t currentQP = response.begin()["qp"].as<uint64_t>();
-	currentQP += delta;
-	
-	transaction.exec("UPDATE player SET qp = " + std::to_string(currentQP) + " WHERE userid = " + transaction.quote(userID) + ";");
-	transaction.commit();
+	try {
+		transaction.exec("UPDATE player SET qp = qp + " + std::to_string(delta) + " WHERE userid = " + transaction.quote(userID) + ";");
+		transaction.commit();
+	} catch (...) {
+		transaction.abort();
+	}
 }
 
 std::string getUserLocation(const std::string& userID)
@@ -55,14 +58,53 @@ std::string getUserLocation(const std::string& userID)
 	pqxx::work transaction{*dbConn};
 	pqxx::result response = transaction.exec("SELECT * FROM player WHERE userid = " + transaction.quote(userID) + ";");
 	
-	return response.begin()["location"].c_str();
+	return response.empty()? "" : response.begin()["location"].c_str();
 }
 
 void setUserLocation(const std::string& userID, const std::string& newLocation)
 {
 	pqxx::work transaction{*dbConn};
-	transaction.exec("UPDATE player SET location = " + transaction.quote(newLocation) + " WHERE userid = " + transaction.quote(userID) + ";");
-	transaction.commit();
+	try {
+		transaction.exec("UPDATE player SET location = " + transaction.quote(newLocation) + " WHERE userid = " + transaction.quote(userID) + ";");
+		transaction.commit();
+	} catch (...) {
+		transaction.abort();
+	}
+}
+
+void alterInventoryItemCount(const std::string& userID, const std::string& itemLabel, int delta)
+{
+	pqxx::work transaction{*dbConn};
+	
+	try {
+		pqxx::result foundItems = transaction.exec("SELECT * FROM inventory inv JOIN player p ON(inv.userid, p.id) JOIN item i ON(inv.itemid, i.id) WHERE p.userid = " + transaction.quote(userID) + " AND itemid = " + transaction.quote(itemLabel) + ";");
+		
+		if (foundItems.empty()) {
+			//add the item
+			transaction.exec("INSERT INTO inventory(userid, itemid, amount) VALUES((SELECT id FROM player WHERE userid = " + transaction.quote(userID) + "), (SELECT id FROM item WHERE label = " + transaction.quote(itemLabel) + "), " + std::to_string(delta) + ");");
+		} else {
+			//item already there, update it's count
+			uint64_t newAmount = foundItems.begin()["amount"].as<uint64_t>() + delta;
+			
+			if (newAmount > 0) {
+				transaction.exec("UPDATE inventory SET amount = " + std::to_string(newAmount) + " FROM player p, item i WHERE p.userid = " + transaction.quote(userID) + " AND i.label = " + transaction.quote(itemLabel) + ";");
+			} else {
+				transaction.exec("DELETE FROM inventory inv USING player p, item i WHERE inv.userid = p.id AND inv.itemid = i.id AND p.userid = " + transaction.quote(userID) + " AND i.label = " + transaction.quote(itemLabel) + ";");
+			}
+		}
+		
+		transaction.commit();
+	} catch (...) {
+		transaction.abort();
+	}
+}
+
+uint64_t getInventoryItemCount(const std::string& userID, const std::string& itemLabel)
+{
+	pqxx::work transaction{*dbConn};
+	pqxx::result itemAmounts = transaction.exec("SELECT inv.amount FROM inventory inv JOIN player p ON(inv.userid = p.id) JOIN item i ON(inv.itemid = i.id) WHERE p.userid = " + transaction.quote(userID) + " AND i.label = " + transaction.quote(itemLabel) + ";");
+	
+	return itemAmounts.empty()? 0 : itemAmounts.begin()["amount"].as<uint64_t>();
 }
 
 } // namespace Database
--- core/database.h
+++ core/database.h
@@ -6,7 +6,7 @@ namespace Database
 //connects to the Database. This should only be called a single time at the very beginning to initialize that connection.
 void dbConnect();
 //this creates a player. If the player already exists, it currently exits without creating them or throwing an error.
-void createPlayer(const std::string& userID);
+void createPlayer(const std::string& userID, int qpCount, std::string location);
 //returns a user's qp count.
 uint64_t getUserQpCount(const std::string& userID);
 //returns a user's location
@@ -17,4 +17,10 @@ std::string getUserLocation(const std::string& userID);
 void alterUserQpCount(const std::string& userID, int64_t delta);
 //sets a user's location to the specified string.
 void setUserLocation(const std::string& userID, const std::string& newLocation);
+//Alters how many of a specific item a user has. If the item does not exist yet, an entry will be created for it with the specified amount.
+//If the item does exist the amount indicates how many of the item to add or remove from a player's inventory.
+//Should this leave the item at an amount of 0 OR LESS, its entry will be removed from the DB.
+void alterInventoryItemCount(const std::string& userID, const std::string& itemLabel, int amount);
+//retrieves the amount a player has of an item in their inventory.
+uint64_t getInventoryItemCount(const std::string& userID, const std::string& itemLabel);
 }
