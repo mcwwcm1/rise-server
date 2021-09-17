4dd041dd193df84e21c0450f780413af0102a01a
Psychpsyo <psychpsyo@gmail.com>
2021-09-17 12:54:29 +0200

    missed db.h in last commit

--- Core/Database.h
+++ Core/Database.h
@@ -5,25 +5,25 @@ namespace Database
 {
 //connects to the Database. This should only be called a single time at the very beginning to initialize that connection.
 void DbConnect();
-//this creates a player (or crew). If the player/crew already exists, it currently exits without creating them or throwing an error.
+//this creates a player. If the player already exists, it currently exits without creating them or throwing an error.
 void CreatePlayer(const std::string& userID, int qpCount, std::string location);
-//returns a user's (or crew's) qp count.
+//returns a user's qp count.
 uint64_t GetUserQpCount(const std::string& userID);
 //returns a user's location
 std::string GetUserLocation(const std::string& userID);
-//alters a user's (or crew's) qp count. The passed delta is how many qpies to add/subtract from their current balance.
+//alters a user's qp count. The passed delta is how many qpies to add/subtract from their current balance.
 //Positive values are added, negative ones subtracted.
 //NOTE: This does not do bounds checking. If used incorrectly, this can make a user's qp count uder- or overflow.
 void AlterUserQpCount(const std::string& userID, int64_t delta);
 //sets a user's location to the specified string.
 void SetUserLocation(const std::string& userID, const std::string& newLocation);
-//Alters how many of a specific item a user/crew has. If the item does not exist yet, an entry will be created for it with the specified amount.
-//If the item does exist the amount indicates how many of the item to add or remove from a player's (or crew's) inventory.
+//Alters how many of a specific item a user has. If the item does not exist yet, an entry will be created for it with the specified amount.
+//If the item does exist the amount indicates how many of the item to add or remove from a player's inventory.
 //Should this leave the item at an amount of 0 OR LESS, its entry will be removed from the DB.
 void AlterInventoryItemCount(const std::string& userID, const std::string& itemLabel, int amount);
-//retrieves the amount a player/crew has of an item in their inventory.
+//retrieves the amount a player has of an item in their inventory.
 uint64_t GetInventoryItemCount(const std::string& userID, const std::string& itemLabel);
-//deletes a player/crew and all associated data from the database.
+//deletes a player and all associated data from the database.
 //this is irreversible.
 void DeletePlayer(const std::string& userID);
 //Appends user IDs and their qp counts, sorted by qp count in descending order to the two passed vectors.
@@ -43,4 +43,12 @@ void AddUserToCrew(const std::string& userID, const std::string& crewID);
 void RemoveUserFromCrew(const std::string& userID, const std::string& crewID);
 //returns an std::vector of strings. These strings are the crew IDs that the passed user is in.
 std::vector<std::string> GetUserCrews(const std::string& userID);
+
+//crew versions of user functions above
+void CreateCrew(const std::string& userID, int qpCount, std::string location);
+uint64_t GetCrewQpCount(const std::string& userID);
+void AlterCrewQpCount(const std::string& userID, int64_t delta);
+void AlterCrewInventoryItemCount(const std::string& userID, const std::string& itemLabel, int amount);
+uint64_t GetCrewInventoryItemCount(const std::string& userID, const std::string& itemLabel);
+void DeleteCrew(const std::string& userID);
 }
